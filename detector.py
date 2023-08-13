cd76f12119f01dcc75c1d93dffada4c5ddf13bac
Mike <michael.naughton@gmail.com>
2023-08-13 16:40:55 +1000

    add comments

--- detector.py
+++ detector.py
@@ -14,9 +14,15 @@ Path("training").mkdir(exist_ok=True)
 Path("output").mkdir(exist_ok=True)
 Path("validation").mkdir(exist_ok=True)
 
+
 def encode_known_faces(
     model: str = "hog", encodings_location: Path = DEFAULT_ENCODINGS_PATH
 ) -> None:
+    """
+    Encodes the faces contained in ./training/{label}/img_{num}.jpeg
+    and storing them in a pickle file in ./output.
+    Only needs to be run when the initial training set is created or modified.
+    """
     names = []
     encodings = []
     for filepath in Path("training").glob("*/*"):
@@ -34,29 +40,29 @@ def encode_known_faces(
     with encodings_location.open(mode="wb") as f:
         pickle.dump(name_encodings, f)
 
-encode_known_faces()
-
 
 def recognize_faces(
     image_location: str,
     model: str = "hog",
     encodings_location: Path = DEFAULT_ENCODINGS_PATH,
 ) -> None:
+    """
+    Load unknown files and classifies them using the encoding created from
+    encode_known_faces.
+    """
     with encodings_location.open(mode="rb") as f:
         loaded_encodings = pickle.load(f)
 
     input_image = face_recognition.load_image_file(image_location)
 
-    input_face_locations = face_recognition.face_locations(
-        input_image, model=model
-    )
+    input_face_locations = face_recognition.face_locations(input_image, model=model)
     input_face_encodings = face_recognition.face_encodings(
         input_image, input_face_locations
     )
 
     pillow_image = Image.fromarray(input_image)
     draw = ImageDraw.Draw(pillow_image)
-    
+
     for bounding_box, unknown_encoding in zip(
         input_face_locations, input_face_encodings
     ):
@@ -69,24 +75,29 @@ def recognize_faces(
     del draw
     pillow_image.show()
 
+
 def _recognize_face(unknown_encoding, loaded_encodings):
+    """
+    Compares the unknown encoding to the known encodings and returns the name.
+    Returns the most highest voted name.
+    """
     boolean_matches = face_recognition.compare_faces(
         loaded_encodings["encodings"], unknown_encoding
     )
     votes = Counter(
-        name
-        for match, name in zip(boolean_matches, loaded_encodings["names"])
-        if match
+        name for match, name in zip(boolean_matches, loaded_encodings["names"]) if match
     )
     if votes:
         return votes.most_common(1)[0][0]
-    
+
+
 def _display_face(draw, bounding_box, name):
+    """
+    Draws the bounding box and the name of the face.
+    """
     top, right, bottom, left = bounding_box
     draw.rectangle(((left, top), (right, bottom)), outline=BOUNDING_BOX_COLOR)
-    text_left, text_top, text_right, text_bottom = draw.textbbox(
-        (left, bottom), name
-    )
+    text_left, text_top, text_right, text_bottom = draw.textbbox((left, bottom), name)
     draw.rectangle(
         ((text_left, text_top), (text_right, text_bottom)),
         fill="blue",
@@ -98,12 +109,15 @@ def _display_face(draw, bounding_box, name):
         fill="white",
     )
 
+
 def validate(model: str = "hog"):
+    """
+    Validates the model by running it on the images in the validation folder.
+    """
     for filepath in Path("validation").rglob("*"):
         if filepath.is_file():
-            recognize_faces(
-                image_location=str(filepath.absolute()), model=model
-            )
+            recognize_faces(image_location=str(filepath.absolute()), model=model)
+
 
 # Removed recognize_faces("unknown.jpg")
 validate()
