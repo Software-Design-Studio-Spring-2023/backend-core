8cfc09341d14e7cde36b7a09e474b3e781a00651
Mike <michael.naughton@gmail.com>
2023-08-15 23:05:38 +1000

    create client that streams video and a server that can process it

--- server/main.py
+++ server/main.py
@@ -1,4 +1,4 @@
-import socketio
+from flask_socketio import SocketIO, emit, join_room
 import websockets
 import asyncio
 import threading
@@ -10,9 +10,30 @@ from infrastructure.FaceDetector import FaceDetector as FaceDetectorClass
 FaceDetector = FaceDetectorClass()
 FaceEncoder = FaceEncoderClass()
 app = Flask(__name__)
-sio = socketio.Client()
+app.config['SECRET'] = 'password'
+socketio = SocketIO(app, cors_allowed_origins="*")
 
+active_sessions = {}
 
+@socketio.on('connect')
+def handle_connect():
+    active_sessions[request.sid] = None
+    join_room(request.sid)
+
+@socketio.on('disconnect')
+def handle_disconnect():
+    if request.sid in active_sessions:
+        del active_sessions[request.sid]
+
+def ack():
+    print('message was received!')
+
+@socketio.on('frame')
+def handle_frame(frame_data): # TODO: get client id and send to that client only, and the host application
+     if request.sid in active_sessions:
+        print('frame received')
+        emit('frame_data', frame_data, broadcast=True, callback=ack)
+        
 @app.route("/", methods=["GET", "POST"])
 def welcome():
     return "Hello World!"
@@ -63,4 +84,5 @@ def add():
 
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=8080)
+
+    socketio.run(app, host="127.0.0.1", port=8080)
